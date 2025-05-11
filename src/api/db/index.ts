e061519e8a1656a7bf162e836e8890bab7c3f400
agustin <agustin.ryo@gmail.com>
2025-05-11 15:15:57 +0200

    feat: api: configurción de la conexión v2

--- src/api/db/index.ts
+++ src/api/db/index.ts
@@ -1,8 +1,20 @@
-import mysql from "mysql2/promise";
+import { createConnection } from "mysql2/promise";
 import { drizzle } from 'drizzle-orm/mysql2';
 import * as schema from './schema';
 
-const dbPath = process.env.DATABASE_URL;
+const dbUser = process.env.DATABASE_USER;
+const dbPassword = process.env.DATABASE_PASSWORD;
+const dbHost = process.env.DATABASE_HOST;
+const dbPort = process.env.DATABASE_PORT;
+const dbName = process.env.DATABASE_NAME;
 
-const connection = await mysql.createConnection(dbPath);
+const connection = await createConnection({
+  host: dbHost,
+  user: dbUser,
+  database: dbName,
+  password: dbPassword,
+  port: parseInt(dbPort),
+  Promise: globalThis.Promise
+});
+
 export const db = drizzle(connection, { schema: schema, mode: 'default' });

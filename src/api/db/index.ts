0a7b55240d6416eb1ad267294d60e75f4be9f0ee
agustin <agustin.ryo@gmail.com>
2025-05-11 20:23:55 +0200

    feat: api: configurción de la conexión final

--- src/api/db/index.ts
+++ src/api/db/index.ts
@@ -3,11 +3,6 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import * as schema from './schema';
 
 const dbPath = process.env.DATABASE_URL;
-const dbUser = process.env.DATABASE_USER;
-const dbPassword = process.env.DATABASE_PASSWORD;
-const dbHost = process.env.DATABASE_HOST;
-const dbPort = process.env.DATABASE_PORT;
-const dbName = process.env.DATABASE_NAME;
 
 const connection = mysql.createPool({
   uri: dbPath,

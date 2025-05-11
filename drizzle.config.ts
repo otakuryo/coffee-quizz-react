df2d218a4d606c38b4f42fc7c5b26bf2be485c66
agustin <agustin.ryo@gmail.com>
2025-05-11 13:01:01 +0200

    feat: db: actualización de fichero de esquema en drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: 'mysql', // 'mysql' | 'sqlite' | 'turso'
-  schema: './src/api/db/migrate'
+  schema: './src/api/db/migrate.ts'
 })

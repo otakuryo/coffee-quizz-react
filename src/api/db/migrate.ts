44c0b199cb5ed72453950591b9848bf1d3d2a0e9
agustin <agustin.ryo@gmail.com>
2025-05-11 13:20:36 +0200

    feat: db: actualización de fichero de esquema en drizzle v3

--- src/api/db/migrate.ts
+++ src/api/db/migrate.ts
@@ -1,10 +1,12 @@
+import mysql from "mysql2/promise";
 import { migrate } from "drizzle-orm/mysql2/migrator";
-
 import { drizzle } from "drizzle-orm/mysql2";
 
 const dbPath = process.env.DATABASE_URL;
-const db = drizzle(dbPath);
+
 (async () => {
+  const connection = await mysql.createConnection(dbPath);
+  const db = drizzle(connection);
   await migrate(db, { migrationsFolder: "./drizzle" });
   process.exit();
 })();
